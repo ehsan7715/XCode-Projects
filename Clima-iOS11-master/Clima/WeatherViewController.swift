2393c81724ec42f475ca69c75c079c6044aa7ea4
ehsan7715 <eh.taghipour@icloud.com>
2017-10-27 12:34:20 +0330

    pod files

--- Clima-iOS11-master/Clima/WeatherViewController.swift
+++ Clima-iOS11-master/Clima/WeatherViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController,CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -17,6 +18,8 @@ class WeatherViewController: UIViewController {
     
 
     //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
+    
     
 
     
@@ -32,6 +35,9 @@ class WeatherViewController: UIViewController {
         
         //TODO:Set up the location manager here.
     
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         
         
     }
